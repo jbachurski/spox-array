d2ae2220d5adf280ab291711173111db5bcf2152
jbachurski <kbachurski@gmail.com>
2023-03-15 16:07:06 +0000

    Implement mean

--- src/spox_array/_func.py
+++ src/spox_array/_func.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Iterable, Sequence
 
 import spox.opset.ai.onnx.v17 as op
 from spox import Var
@@ -13,3 +13,14 @@ from ._array import handle_out, implements, promote_args, unwrap_vars, wrap_var
 @unwrap_vars
 def concatenate(arrays: Sequence[Var], axis: int = 0):
     return op.concat(arrays, axis=axis)
+
+
+@implements
+@handle_out
+@wrap_var
+@promote_args
+@unwrap_vars
+def mean(var: Var, axis: int | tuple[int, ...] = 0, keepdims: bool = False):
+    if not isinstance(axis, Iterable):
+        axis = (axis,)
+    return op.reduce_mean(var, axes=axis, keepdims=keepdims)
