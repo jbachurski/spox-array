2b554d75cd00ed8095242d845bc4235ccd9daf73
jbachurski <kbachurski@gmail.com>
2023-03-16 11:49:51 +0000

    Add more tests

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -5,3 +5,16 @@ from spox_array.testing import assert_equiv_prop
 
 def test_concat():
     assert_equiv_prop(np.concatenate, [np.array([1]), np.array([2])])
+
+
+def test_transpose():
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]))
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]), axis=(0, 1))
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]), axis=(1, 0))
+
+
+def test_mean():
+    assert_equiv_prop(np.mean, np.array([1, 3, 6, 8]))
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]), axis=(0, 1))
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]), axis=0)
+    assert_equiv_prop(np.mean, np.array([[1, 3], [6, 8]]), axis=1)
